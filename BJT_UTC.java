c26192539b558e62f2415022c4137283cb294ff0
George King <georgeking1208@outlook.com>
2017-01-06 13:26:33 +0800

    creat a code file
    creat a code file, and sperate the original file into two part, one is
    recording file, other is java code file.

---
+++ BJT_UTC.java
@@ -0,0 +1,24 @@
+package hello;
+	
+import java.util.Scanner;
+	
+public class BJT_UTC {
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		//原始数字/100得到的余数是分钟，得到的商就是小时数；不过要在注意的是跨日的时间换算；
+		//如果北京时间为0-7；则伦敦标准时是北京时间-8+24，，如果北京时间大于7，则直接减去8即可；
+		Scanner in = new Scanner(System.in);
+		System.out.print("请输入北京时间，本程序将会帮你换算为伦敦标准时:");
+		int BJT = in.nextInt();
+		int UTC = 0; 
+		int h = BJT/100;
+		int min = BJT%100;
+		if(h>7){
+			UTC=(h-8)*100+min;
+		}else{
+			UTC=(h-8+24)*100+min;
+		}
+		System.out.println("伦敦标准时为："+UTC);
+	}
+}
